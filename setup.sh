1b91fdd0254573bc2261ce90adc1de377b054be3
dmcelectrico <dmcelectrico@gmail.com>
2014-08-01 14:06:04 +0200

    Adding piDA dependency to setup.sh
    Watch out for non-unix file endings.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-#								#
-#		INSTALLS ALL SOFTWARE DEPENDENCIES		#
-#	  tested in a fresh raspbian setup via NOOBS		#
-#								#
+#													#
+#		INSTALLS ALL SOFTWARE DEPENDENCIES			#
+#	  tested in a fresh raspbian setup via NOOBS	#
+#													#
 
 sudo apt-get update
-sudo apt-get install -y python-wxtools python-matplotlib
+sudo apt-get install -y python-wxtools python-matplotlib git
+echo "Installing now piDA lib..."
+cd /tmp/
+git clone https://github.com/noeldiazro/piDA.git
+cd ./piDA
+./setup.sh --no-update
+cd
+echo "Cleaning up..."
+sudo rm -r /tmp/piDA
