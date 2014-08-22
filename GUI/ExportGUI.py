3ee4d1668eaa9b77198765a4ac7ec4bed5177cc3
dmcelectrico <dmcelectrico@gmail.com>
2014-08-22 12:54:46 +0200

    ExportGUI (csv) and layout done for each channel.
    TODO: For all channels with timestamp merging.

--- GUI/ExportGUI.py
+++ GUI/ExportGUI.py
@@ -24,17 +24,129 @@ class ExportGUI(wx.Panel):
 
 
 		#First column 
+		hbox11 = wx.BoxSizer(wx.HORIZONTAL)
+		self.TC1title= wx.TextCtrl(self)
+		self.TC1title.SetValue(self.mainFrame.get_plot_title())
+		hbox11.Add(wx.StaticText(self,-1,"Title:	"),flag=wx.ALIGN_LEFT| wx.ALL,border=1)
+		hbox11.Add(self.TC1title,flag=wx.ALIGN_RIGHT)
 
+		hbox12 = wx.BoxSizer(wx.HORIZONTAL)
+		self.TCx1Label= wx.TextCtrl(self)
+		self.TCx1Label.SetValue(self.mainFrame.get_xlabel())
+		hbox12.Add(wx.StaticText(self,-1,"X Label:	"),flag=wx.ALIGN_LEFT| wx.ALL,border=1)
+		hbox12.Add(self.TCx1Label,flag=wx.ALIGN_RIGHT)
+
+		hbox13 = wx.BoxSizer(wx.HORIZONTAL)
+		self.TCy1Label= wx.TextCtrl(self)
+		self.TCy1Label.SetValue(self.mainFrame.get_ylabel())
+		hbox13.Add(wx.StaticText(self,-1,"Y Label:	"),flag=wx.ALIGN_LEFT| wx.ALL,border=1)
+		hbox13.Add(self.TCy1Label,flag=wx.ALIGN_RIGHT)
+
+		hbox14 = wx.BoxSizer(wx.HORIZONTAL)
+		self.BcsvCh0 = wx.Button(self,0,"Export CSV")
+		self.BpdfCh0 = wx.Button(self,-1,"Export PDF")
+		hbox14.Add(self.BcsvCh0,flag=wx.ALIGN_CENTER|wx.ALL,border=1)
+		hbox14.Add(self.BpdfCh0,flag=wx.ALIGN_CENTER|wx.ALL,border=1)
+
+
+		vbox1.Add(hbox11,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
+		vbox1.Add(hbox12,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
+		vbox1.Add(hbox13,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
+		vbox1.Add(hbox14,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
 
 		#Second column
-		
+		hbox21 = wx.BoxSizer(wx.HORIZONTAL)
+		self.TC2title= wx.TextCtrl(self)
+		self.TC2title.SetValue(self.mainFrame.get_plot_title())
+		hbox21.Add(wx.StaticText(self,-1,"Title:	"),flag=wx.ALIGN_LEFT| wx.ALL,border=1)
+		hbox21.Add(self.TC2title,flag=wx.ALIGN_RIGHT)
+
+		hbox22 = wx.BoxSizer(wx.HORIZONTAL)
+		self.TCx2Label= wx.TextCtrl(self)
+		self.TCx2Label.SetValue(self.mainFrame.get_xlabel())
+		hbox22.Add(wx.StaticText(self,-1,"X Label:	"),flag=wx.ALIGN_LEFT| wx.ALL,border=1)
+		hbox22.Add(self.TCx2Label,flag=wx.ALIGN_RIGHT)
+
+		hbox23 = wx.BoxSizer(wx.HORIZONTAL)
+		self.TCy2Label= wx.TextCtrl(self)
+		self.TCy2Label.SetValue(self.mainFrame.get_ylabel())
+		hbox23.Add(wx.StaticText(self,-1,"Y Label:	"),flag=wx.ALIGN_LEFT| wx.ALL,border=1)
+		hbox23.Add(self.TCy2Label,flag=wx.ALIGN_RIGHT)
+
+		hbox24 = wx.BoxSizer(wx.HORIZONTAL)
+		self.BcsvCh1 = wx.Button(self,1,"Export CSV")
+		self.BpdfCh1 = wx.Button(self,-1,"Export PDF")
+		hbox24.Add(self.BcsvCh1,flag=wx.ALIGN_CENTER|wx.ALL,border=1)
+		hbox24.Add(self.BpdfCh1,flag=wx.ALIGN_CENTER|wx.ALL,border=1)
+
+
+		vbox2.Add(hbox21,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
+		vbox2.Add(hbox22,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
+		vbox2.Add(hbox23,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
+		vbox2.Add(hbox24,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
 		
 
 		#Third column
-		
+		hbox31 = wx.BoxSizer(wx.HORIZONTAL)
+		self.TC3title= wx.TextCtrl(self)
+		self.TC3title.SetValue(self.mainFrame.get_plot_title())
+		hbox31.Add(wx.StaticText(self,-1,"Title:	"),flag=wx.ALIGN_LEFT| wx.ALL,border=1)
+		hbox31.Add(self.TC3title,flag=wx.ALIGN_RIGHT)
+
+		hbox32 = wx.BoxSizer(wx.HORIZONTAL)
+		self.TCx3Label= wx.TextCtrl(self)
+		self.TCx3Label.SetValue(self.mainFrame.get_xlabel())
+		hbox32.Add(wx.StaticText(self,-1,"X Label:	"),flag=wx.ALIGN_LEFT| wx.ALL,border=1)
+		hbox32.Add(self.TCx3Label,flag=wx.ALIGN_RIGHT)
+
+		hbox33 = wx.BoxSizer(wx.HORIZONTAL)
+		self.TCy3Label= wx.TextCtrl(self)
+		self.TCy3Label.SetValue(self.mainFrame.get_ylabel())
+		hbox33.Add(wx.StaticText(self,-1,"Y Label:	"),flag=wx.ALIGN_LEFT| wx.ALL,border=1)
+		hbox33.Add(self.TCy3Label,flag=wx.ALIGN_RIGHT)
+
+		hbox34 = wx.BoxSizer(wx.HORIZONTAL)
+		self.BcsvCh2 = wx.Button(self,2,"Export CSV")
+		self.BpdfCh2 = wx.Button(self,-1,"Export PDF")
+		hbox34.Add(self.BcsvCh2,flag=wx.ALIGN_CENTER|wx.ALL,border=1)
+		hbox34.Add(self.BpdfCh2,flag=wx.ALIGN_CENTER|wx.ALL,border=1)
+
+
+		vbox3.Add(hbox31,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
+		vbox3.Add(hbox32,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
+		vbox3.Add(hbox33,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
+		vbox3.Add(hbox34,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
 
 		#Fourth column
-		
+		hbox41 = wx.BoxSizer(wx.HORIZONTAL)
+		self.TC4title= wx.TextCtrl(self)
+		self.TC4title.SetValue(self.mainFrame.get_plot_title())
+		hbox41.Add(wx.StaticText(self,-1,"Title:	"),flag=wx.ALIGN_LEFT| wx.ALL,border=1)
+		hbox41.Add(self.TC4title,flag=wx.ALIGN_RIGHT)
+
+		hbox42 = wx.BoxSizer(wx.HORIZONTAL)
+		self.TCx4Label= wx.TextCtrl(self)
+		self.TCx4Label.SetValue(self.mainFrame.get_xlabel())
+		hbox42.Add(wx.StaticText(self,-1,"X Label:	"),flag=wx.ALIGN_LEFT| wx.ALL,border=1)
+		hbox42.Add(self.TCx4Label,flag=wx.ALIGN_RIGHT)
+
+		hbox43 = wx.BoxSizer(wx.HORIZONTAL)
+		self.TCy4Label= wx.TextCtrl(self)
+		self.TCy4Label.SetValue(self.mainFrame.get_ylabel())
+		hbox43.Add(wx.StaticText(self,-1,"Y Label:	"),flag=wx.ALIGN_LEFT| wx.ALL,border=1)
+		hbox43.Add(self.TCy4Label,flag=wx.ALIGN_RIGHT)
+
+		hbox44 = wx.BoxSizer(wx.HORIZONTAL)
+		self.BcsvCh3 = wx.Button(self,3,"Export CSV")
+		self.BpdfCh3 = wx.Button(self,-1,"Export PDF")
+		hbox44.Add(self.BcsvCh3,flag=wx.ALIGN_CENTER|wx.ALL,border=1)
+		hbox44.Add(self.BpdfCh3,flag=wx.ALIGN_CENTER|wx.ALL,border=1)
+
+
+		vbox4.Add(hbox41,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
+		vbox4.Add(hbox42,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
+		vbox4.Add(hbox43,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
+		vbox4.Add(hbox44,flag=wx.ALIGN_CENTER | wx.ALL,border=1)
 
 		#Last row
 		hbox5.Add(wx.Button(self,-1,"ojete"))
@@ -48,8 +160,89 @@ class ExportGUI(wx.Panel):
 		vbox.Add(hbox5,0,flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.DOWN,border=2)
 		self.SetSizer(vbox)
 
+		#Bindings
+
+		self.BcsvCh0.Bind(wx.EVT_BUTTON,self.OnExportChannelToCSV)
+		self.BcsvCh1.Bind(wx.EVT_BUTTON,self.OnExportChannelToCSV)
+		self.BcsvCh2.Bind(wx.EVT_BUTTON,self.OnExportChannelToCSV)
+		self.BcsvCh3.Bind(wx.EVT_BUTTON,self.OnExportChannelToCSV)
 
 
+
+	def SaveFileDialog(self):
+		"""Opens a File Dialog for file saving and returns the file path chosen"""
+		saveFileDialog = wx.FileDialog(self, "Export as CSV", "", "","CSV (*.csv)|*.csv", wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
+		if saveFileDialog.ShowModal() == wx.ID_CANCEL:
+			return     # the user changed idea...
+		filepath = saveFileDialog.GetPath()
+		#This conditional checks if the file extension was added to the filename.
+		if filepath[-4:].lower() != ".csv":
+			filepath += ".csv"
+		return filepath
+
+	def UpdateChannelWidgets(self,channel,enable):
+		if(channel==0):
+			if(enable):
+				self.TC1title.Enable()
+				self.TCx1Label.Enable()
+				self.TCy1Label.Enable()
+				self.BcsvCh0.Enable()
+				self.BpdfCh0.Enable()
+			else:
+				self.TC1title.Disable()
+				self.TCx1Label.Disable()
+				self.TCy1Label.Disable()
+				self.BcsvCh0.Disable()
+				self.BpdfCh0.Disable()
+		elif(channel==1):
+			if(enable):
+				self.TC2title.Enable()
+				self.TCx2Label.Enable()
+				self.TCy2Label.Enable()
+				self.BcsvCh1.Enable()
+				self.BpdfCh1.Enable()
+			else:
+				self.TC2title.Disable()
+				self.TCx2Label.Disable()
+				self.TCy2Label.Disable()
+				self.BcsvCh1.Disable()
+				self.BpdfCh1.Disable()
+		elif(channel==2):
+			if(enable):
+				self.TC3title.Enable()
+				self.TCx3Label.Enable()
+				self.TCy3Label.Enable()
+				self.BcsvCh2.Enable()
+				self.BpdfCh2.Enable()
+			else:
+				self.TC3title.Disable()
+				self.TCx3Label.Disable()
+				self.TCy3Label.Disable()
+				self.BcsvCh2.Disable()
+				self.BpdfCh2.Disable()
+		elif(channel==3):
+			if(enable):
+				self.TC4title.Enable()
+				self.TCx4Label.Enable()
+				self.TCy4Label.Enable()
+				self.BcsvCh3.Enable()
+				self.BpdfCh3.Enable()
+			else:
+				self.TC4title.Disable()
+				self.TCx4Label.Disable()
+				self.TCy4Label.Disable()
+				self.BcsvCh3.Disable()
+				self.BpdfCh3.Disable()
+
+		#PDF export won't be available for a while, so no point to enable the button
+		self.BpdfCh0.Disable()
+		self.BpdfCh1.Disable()
+		self.BpdfCh2.Disable()
+		self.BpdfCh3.Disable()
+
+
+
+		
 		# # # # # # # # # # # # # #	#
 		#							#
 		#	E	V	E	N	T	S	#
@@ -58,11 +251,29 @@ class ExportGUI(wx.Panel):
 
 	def OnExportAll(self,e):
 		"""Exports all the data to one file. 
-			*NOT FINISHED* *EXAMPLE OF HOW TO EXPORT TO CSV FROM CHANNEL 0 DATA*
+			TODO
 		"""
-		saveFileDialog = wx.FileDialog(self, "Abrir fichero CSV", "", "","CSV (*.csv)|*.csv", wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
-		if saveFileDialog.ShowModal() == wx.ID_CANCEL:
-			return     # the user changed idea...
-		output = Writer(saveFileDialog.GetPath())
-		output.writerows(self.mainFrame.acqPanel0.Module.get_data())
-		
+		pass
+
+	def OnExportChannelToCSV(self,e):
+		"""Exports channel data to a csv file."""
+		filepath = self.SaveFileDialog()
+		if filepath=="":
+			return
+		output = Writer(filepath)
+		if (e.GetId() == 0):
+			output.writerow([self.TC1title.GetValue()])
+			output.writerow([self.TCx1Label.GetValue(),self.TCy1Label.GetValue()])
+			output.writerows(self.mainFrame.acqPanel0.Module.get_data())
+		elif (e.GetId() == 1):
+			output.writerow([self.TC2title.GetValue()])
+			output.writerow([self.TCx2Label.GetValue(),self.TCy2Label.GetValue()])
+			output.writerows(self.mainFrame.acqPanel1.Module.get_data())
+		elif (e.GetId() == 2):
+			output.writerow([self.TC3title.GetValue()])
+			output.writerow([self.TCx3Label.GetValue(),self.TCy3Label.GetValue()])
+			output.writerows(self.mainFrame.acqPanel1.Module.get_data())
+		elif (e.GetId() == 3):
+			output.writerow([self.TC4title.GetValue()])
+			output.writerow([self.TCx4Label.GetValue(),self.TCy4Label.GetValue()])
+			output.writerows(self.mainFrame.acqPanel1.Module.get_data())
